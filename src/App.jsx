a7198114ec8389069b2bee0d3637f67a9f3846d1
Priyanshu Singh <priyanshusingh@Priyanshus-MacBook-Pro-2.local>
2025-05-25 16:16:16 +0530

    added the whole game logic for offline play

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import Gameboard from "./components/Gameboard";
+import StatusBar from './components/StatusBar';
+import { checkWinner } from './gametype/offline/gameUtils';
+
 import "./App.css";
 
 function App() {
   const [mode, setMode] = useState(null); // "offline" | "online" | null
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+  const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
@@ -12,13 +17,26 @@ function App() {
     setWinner(null);
   };
   const handleClick = (index) => {
-    console.log("Clicked index:", index);
+    if (board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = isXNext ? 'X' : 'O';
+    setBoard(newBoard);
+
+    const gameWinner = checkWinner(newBoard);
+    if (gameWinner) {
+      setWinner(gameWinner);
+    } else if (newBoard.every(Boolean)) {
+      setWinner('Draw');
+    } else {
+      setIsXNext(!isXNext);
+    }
   };
 
   const handleModeSelect = (selectedMode) => {
     setMode(selectedMode);
   };
-  
+
   const renderModeSelection = () => (
     <div className="mode-selection">
       <h2>Select Game Mode</h2>
@@ -35,10 +53,16 @@ function App() {
     return (
       <div className="app">
         <h1>Tic Tac Toe</h1>
+        <StatusBar isXNext={isXNext} winner={winner} />
         <Gameboard board={board} onClick={handleClick} />
-
         <button onClick={resetGame} className="reset-button">Restart Game</button>
-        <button onClick={() => setMode(null)} className="back-button">Back to Mode Selection</button>
+        <button onClick={() => {
+          setMode(null)
+          setBoard(Array(9).fill(null));
+          setIsXNext(true);
+          setWinner(null);
+
+        }} className="back-button">Back to Mode Selection</button>
       </div>
     );
   }
--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const StatusBar = () => {
-  return (
-    <div>StatusBar</div>
-  )
+const StatusBar = ({ isXNext, winner }) => {
+  let status;
+  if (winner === 'Draw'){
+    status = "It's a Draw!";
+  }else if (winner) {
+    status = `Winner: ${winner}`
+    }else {
+        status = `Next Turn: ${isXNext ? 'X' : 'O'}`
+    };
+
+  return <div className="status">{status}</div>;
 }
 
 export default StatusBar
